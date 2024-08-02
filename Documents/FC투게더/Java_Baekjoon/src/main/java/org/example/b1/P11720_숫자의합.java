2a09b75c506ea246eb6ef0317ceabeedf85c9df2
kmc77 <k_minchoi@naver.com>
2024-08-02 19:18:18 +0900

    p1546 평균구하기

--- Documents/FC투게더/Java_Baekjoon/src/main/java/org/example/b1/P11720_숫자의합.java
+++ Documents/FC투게더/Java_Baekjoon/src/main/java/org/example/b1/P11720_숫자의합.java
@@ -1,22 +1,19 @@
 package org.example.b1;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class P11720_숫자의합 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int N = Integer.parseInt(br.readLine()); // 첫 번째 줄의 숫자 N 읽기
-        String sNum = br.readLine(); // 다음 줄에 있는 숫자 문자열 읽기
-
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        String sNum = sc.next();
+        char[] cNum = sNum.toCharArray();
         int sum = 0;
-        for (int i = 0; i < N; i++) {
-            sum += sNum.charAt(i) - '0'; // 각 숫자를 더하기
+        for (int i = 0; i < cNum.length; i++) {
+            sum += cNum[i] - '0';
         }
-
         System.out.println(sum);
     }
 }
---
+++ Documents/FC투게더/Java_Baekjoon/src/main/java/org/example/b1/P1546_평균구하기.java
@@ -0,0 +1,22 @@
+package org.example.b1;
+
+import java.util.Scanner;
+
+public class P1546_평균구하기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt(); // 입력된 과목의 개수
+
+        long sum = 0; // 점수 합계
+        long max = 0; // 최대 점수
+
+        for (int i = 0; i < N; i++) {
+            int temp = sc.nextInt();
+            if(temp > max) max = temp; // 최대 점수 계산
+            sum = sum + temp; // 합계 계산
+        }
+
+        // 평균 계산 후 출력
+        System.out.println(sum * 100.0 / max / N);
+    }
+}
