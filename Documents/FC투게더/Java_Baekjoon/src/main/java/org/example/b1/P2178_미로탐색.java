4cdeed724c2a5ea71a8687ce6dc0b8a999b1051f
kmc77 <k_minchoi@naver.com>
2024-08-27 17:21:27 +0900

    P2178_미로탐색 + BFS

---
+++ Documents/FC투게더/Java_Baekjoon/src/main/java/org/example/b1/P2178_미로탐색.java
@@ -0,0 +1,22 @@
+package org.example.b1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P2178_미로탐색 {
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+    static boolean[][] visited;
+
+    static int[][] A;
+    static int N, M;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+
+    }
+}
